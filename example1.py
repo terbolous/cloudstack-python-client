640a245e51cf4bef2c4bfd9878f0fb39f623100f
Your Name <you@example.com>
2014-07-23 09:58:07 +0800

    first commit

--- example1.py
+++ example1.py
@@ -3,8 +3,8 @@
 import CloudStack
 
 api = 'http://example.com:8080/client/api'
-apikey = 'API KEY'
-secret = 'API SECRET'
+apikey = 'FhlHSpLcOEiB8wVI6xXH8XfA9nLGzdim5eVlUsVa9LXDIleqsAJA1yubYrEHCduSSsCur6gVoEUmQFctHgxv1g'
+secret = 'YbftfeOwmC3uBvQfYgSXUwkuUuMeca6G7EKvUZJFeZrivjpFwEMGZlXMqn3rCt0YRy9mPtyt-1F2S9s0IFBqKQ'
 
 cloudstack = CloudStack.Client(api, apikey, secret)
 
