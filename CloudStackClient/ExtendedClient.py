1cb40529245f40af24fe407bbea1052319e325b9
Erik Weber <terbolous@gmail.com>
2016-02-15 12:01:28 +0100

    PEP8 fixes in ExtendedClient.py

--- CloudStackClient/ExtendedClient.py
+++ CloudStackClient/ExtendedClient.py
@@ -1,46 +1,44 @@
 from Client import Client
 
+
 class ExtendedClient(Client):
     '''
     This is designed to work with the CloudStack API extension which can be
-    found at https://github.com/jasonhancock/cloudstack-api-extension 
+    found at https://github.com/jasonhancock/cloudstack-api-extension
     '''
 
     def getUserData(self, args={}):
-        if not 'id' in args:
+        if 'id' not in args:
             raise RuntimeError("Missing required argument 'id'")
 
-        return self.request('getUserData', args) 
+        return self.request('getUserData', args)
 
     def listBundles(self, args={}):
-        return self.request('listBundles', args) 
-    
+        return self.request('listBundles', args)
+
     def deployBundle(self, args={}):
-        if not 'bundle' in args:
+        if 'bundle' not in args:
             raise RuntimeError("Missing required argument 'bundle'")
 
-        return self.request('bundle', args) 
+        return self.request('bundle', args)
 
     def listVPCs(self, args={}):
         return self.request('listVPCs', args)
 
     def createVPC(self, args={}):
-        if not 'cidr' in args:
+        if 'cidr' not in args:
             raise RuntimeError("Missing required argument 'cidr'")
-        if not 'displaytext' in args:
+        if 'displaytext' not in args:
             raise RuntimeError("Missing required argument 'displaytext'")
-        if not 'name' in args:
+        if 'name' not in args:
             raise RuntimeError("Missing required argument 'name'")
-        if not 'vpcofferingid' in args:
+        if 'vpcofferingid' not in args:
             raise RuntimeError("Missing required argument 'vpcofferingid'")
-        if not 'zoneid' in args:
+        if 'zoneid' not in args:
             raise RuntimeError("Missing required argument 'zoneid'")
         return self.request('createVPC', args)
 
-
     def deleteVPC(self, args={}):
-        if not 'id' in args:
+        if 'id' not in args:
             raise RuntimeError("Missing required argument 'id'")
         return self.request('deleteVPC', args)
-
-
